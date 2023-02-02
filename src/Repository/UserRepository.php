7482623f397a669c319f8670da5d53f255d96c50
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-02-02 10:24:27 +0100

    #29 : Add search query.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -56,20 +56,29 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->add($user, true);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findUsersByLastName(string $query): array
+    {
+        return $this->createQueryBuilder('u')
+          ->andWhere('u.lastname LIKE :query')
+          ->setParameter('query', '%' . $query . '%')
+          ->getQuery()
+          ->getResult();
+    }
+
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findUsersByFistName(string $query): array
+    {
+        return $this->createQueryBuilder('u')
+          ->andWhere('u.firstname LIKE :query')
+          ->setParameter('query', '%' . $query . '%')
+          ->getQuery()
+          ->getResult();
+    }
 
 //    public function findOneBySomeField($value): ?User
 //    {
